32002caf08aa448e4ed102b8c557047ebbff707d
piyushmalav <piyushmalav85@gmail.com>
2023-11-23 03:55:54 +0530

    swagger is added

--- src/main/java/com/arya/vacinationsystem/VacinationsystemApplication.java
+++ src/main/java/com/arya/vacinationsystem/VacinationsystemApplication.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableSwagger2
 public class VacinationsystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VacinationsystemApplication.class, args);
 	}
-
+//	@Bean
+//	public Docket productApi() {
+//		return new Docket(DocumentationType.SWAGGER_2).select()
+//				.apis(RequestHandlerSelectors.basePackage("com.arya.vacinationsystem.controller")).build();
+//	}
 }
